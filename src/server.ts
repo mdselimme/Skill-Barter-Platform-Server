8200824e4ff1177e8e9457dace97319b6ca03266
Md. Selim <mdselimdev@gmail.com>
2026-02-10 02:25:23 +0600

    add: server closed function

--- src/server.ts
+++ src/server.ts
@@ -2,22 +2,37 @@ import { Server } from "node:http";
 import app from "./app";
 import { envVariables } from "./app/config/env.config";
 
-
-
-
 let server: Server;
 
-
+//function to start the server
 const bootstrap = () => {
     try {
         server = app.listen(envVariables.PORT, () => {
             console.log(`Server is running on port ${envVariables.PORT}`);
         });
     } catch (error) {
-        console.error("Failed to start server:", error);
+        serverShutdown("Failed to start server:", error);
     }
 };
 
 bootstrap();
 
+//function to handle server shutdown
+const serverShutdown = async (message:string, err?:any) => {
+    console.log(`Message : ${message}. Server is closing.`, err);
+    if(server){
+        server.close(()=>{
+            process.exit(1);
+        })
+    }
+    process.exit(1);
+};
 
+//when server is manually stopped
+process.on("SIGINT", () => serverShutdown("SIGINT signal received"));
+//when server is stopped by the system
+process.on("SIGTERM", () => serverShutdown("SIGTERM signal received"));
+//when server is stopped for uncaught exceptions
+process.on("uncaughtException", (err) => serverShutdown("Uncaught exception", err));
+//when server is stopped for unhandled rejections
+process.on("unhandledRejection", (reason) => serverShutdown("Unhandled rejection", reason));
