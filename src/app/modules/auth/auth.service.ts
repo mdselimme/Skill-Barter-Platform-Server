719f29476eddd3a442b8caa30ee3211d662a2402
Md. Selim <mdselimdev@gmail.com>
2026-02-13 15:49:55 +0600

    add login access and refresh token

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -2,6 +2,8 @@
 import bcrypt from "bcrypt";
 import { prisma } from "../../utils/prisma";
 import { Prisma } from "@prisma/client";
+import { generateToken } from "../../utils/jwtToken";
+import { envVariables } from "../../config/env.config";
 
 
 //auth login service
@@ -20,8 +22,17 @@ const authLoginUser = async (payload: Prisma.UserCreateInput) => {
         throw new Error("Invalid password");
     }
 
+    const tokenPayload = {
+        id: result.id,
+        email: result.email,
+        role: result.role,
+    }
+
+    const accessToken = generateToken(tokenPayload, envVariables.JWT_ACCESS_SECRET, envVariables.JWT_ACCESS_EXPIRES);
+    const refreshToken = generateToken(tokenPayload, envVariables.JWT_REFRESH_SECRET, envVariables.JWT_REFRESH_EXPIRES);
+
     const {password:_, ...userWithoutPassword} = result;
-    return userWithoutPassword;
+    return {user:userWithoutPassword, accessToken, refreshToken};
 };
 
 //
