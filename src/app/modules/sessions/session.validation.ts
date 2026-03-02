0436320fa7a11bf144a57bd4de40e7b2377b4a99
Md. Selim <mdselimdev@gmail.com>
2026-03-03 02:50:35 +0600

    validation: session create validation

--- src/app/modules/sessions/session.validation.ts
+++ src/app/modules/sessions/session.validation.ts
@@ -3,8 +3,16 @@ import z from "zod";
 
 //add session validation
 const addSessionZodSchema = z.object({
-
-})
+    skillId: z.uuid({
+        error: "Skill ID is required",
+    }),
+    hours: z.iso.date({
+        error: "Hours must be a valid datetime",
+    }),
+    scheduledAt: z.iso.time({
+        error: "Scheduled At must be a valid datetime",
+    }),
+});
 
 export const SessionValidation = {
     addSessionZodSchema
